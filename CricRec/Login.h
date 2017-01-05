72da9a4a944d1cf21d7c813c3cc14b96fe9a7fe3
Muhammad Ehsan ul Haq <mhaq.bese15seecs@seecs.edu.pk>
2017-01-05 15:54:22 +0500

    Code Refactored a bit, but still looking for a MySQL class, we will see later if it would be the correct thing to do. You can see the changes in the code, The major changes are as follows: 1. The check to see if the username is repeated, for that we have added the following  constraint to the database, Query: alter table admin add constraint un_username_admin unique(username);

--- CricRec/Login.h
+++ CricRec/Login.h
@@ -37,8 +37,8 @@ namespace CricRec {
 		}
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::TextBox^  textBox1;
-	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  UserTextBox;
+	private: System::Windows::Forms::TextBox^  PassTextBox;
 	private: System::Windows::Forms::Button^  button1;
 	protected:
 
@@ -57,8 +57,8 @@ namespace CricRec {
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->UserTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->PassTextBox = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
@@ -80,19 +80,19 @@ namespace CricRec {
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Password";
 			// 
-			// textBox1
+			// UserTextBox
 			// 
-			this->textBox1->Location = System::Drawing::Point(167, 36);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 2;
+			this->UserTextBox->Location = System::Drawing::Point(167, 36);
+			this->UserTextBox->Name = L"UserTextBox";
+			this->UserTextBox->Size = System::Drawing::Size(100, 20);
+			this->UserTextBox->TabIndex = 2;
 			// 
-			// textBox2
+			// PassTextBox
 			// 
-			this->textBox2->Location = System::Drawing::Point(167, 81);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
-			this->textBox2->TabIndex = 3;
+			this->PassTextBox->Location = System::Drawing::Point(167, 81);
+			this->PassTextBox->Name = L"PassTextBox";
+			this->PassTextBox->Size = System::Drawing::Size(100, 20);
+			this->PassTextBox->TabIndex = 3;
 			// 
 			// button1
 			// 
@@ -110,8 +110,8 @@ namespace CricRec {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->PassTextBox);
+			this->Controls->Add(this->UserTextBox);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"Login";
@@ -121,42 +121,36 @@ namespace CricRec {
 
 		}
 #pragma endregion
+
+		private: System::Void validateCred(String^ user, String^ pass) {
+			
+			String^ constring = L"datasource = localhost; port = 3306; username = CricRec; password = cricrec";
+			MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+
+			MySqlCommand^ cmdDataBase = gcnew MySqlCommand("select * from CricRec.admin where username = '" + user + "' and password = sha1('" + pass + "');", conDataBase);
+			MySqlDataReader^ myReader;
+			try {
+				conDataBase->Open();
+				myReader = cmdDataBase->ExecuteReader();
+
+				if (myReader->HasRows > 0) {
+					MessageBox::Show("Connected");
+					MainWindow^ menu = gcnew MainWindow;
+					Hide();
+					menu->ShowDialog();
+				}
+				else {
+					MessageBox::Show("Invalid username or password");
+				}
+			}
+			catch (Exception^ex) {
+				MessageBox::Show(ex->Message);
+			}
+		}
+
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-
-		String^ constring = L"datasource = localhost; port = 3306; username = CricRec; password = cricrec";
-		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
-
-		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("select * from CricRec.admin where username = '" + this->textBox1->Text + "' and password = sha1('" + this->textBox2->Text + "');", conDataBase);
-		MySqlDataReader^ myReader;
-		try {
-			conDataBase->Open();
-			myReader = cmdDataBase->ExecuteReader();
-
-			int count = 0;
-			while (myReader->Read()) {
-
-				count = count + 1;
-			}
-
-			if (count == 1) {
-				MessageBox::Show("Connected");
-				MainWindow^ menu = gcnew MainWindow;
-				Hide();
-				menu->ShowDialog();
-			}
-
-			else if (count > 1) {
-				MessageBox::Show("Multiple username exist.... so access is denied!");
-			}
-
-			else {
-				MessageBox::Show("Invalid username or password");
-			}
-		}
-		catch (Exception^ex) {
-			MessageBox::Show(ex->Message);
-		}
-
+		//A function to validate the username and password.
+		validateCred(this->UserTextBox->Text, this->PassTextBox->Text);
 
 	}
 };
