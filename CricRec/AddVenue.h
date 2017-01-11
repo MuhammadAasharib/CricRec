f886ea38f63b78913aaf421971c955e188324f09
MuhammadAasharib <aasharib@icloud.com>
2017-01-11 14:11:43 +0500

    venue

--- CricRec/AddVenue.h
+++ CricRec/AddVenue.h
@@ -8,6 +8,7 @@ namespace CricRec {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
 
 	/// <summary>
 	/// Summary for AddVenue
@@ -35,9 +36,9 @@ namespace CricRec {
 			}
 		}
 	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::Label^  label2;
+
 	private: System::Windows::Forms::TextBox^  textBox1;
-	private: System::Windows::Forms::TextBox^  textBox2;
+
 	private: System::Windows::Forms::Button^  button1;
 	protected:
 
@@ -55,9 +56,7 @@ namespace CricRec {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
@@ -70,28 +69,12 @@ namespace CricRec {
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Name";
 			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(27, 82);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(16, 13);
-			this->label2->TabIndex = 1;
-			this->label2->Text = L"Id";
-			// 
 			// textBox1
 			// 
 			this->textBox1->Location = System::Drawing::Point(157, 35);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 2;
-			// 
-			// textBox2
-			// 
-			this->textBox2->Location = System::Drawing::Point(157, 74);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
-			this->textBox2->TabIndex = 3;
 			// 
 			// button1
 			// 
@@ -101,6 +84,7 @@ namespace CricRec {
 			this->button1->TabIndex = 4;
 			this->button1->Text = L"ok";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &AddVenue::button1_Click);
 			// 
 			// AddVenue
 			// 
@@ -108,9 +92,7 @@ namespace CricRec {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"AddVenue";
 			this->Text = L"AddVenue";
@@ -119,5 +101,27 @@ namespace CricRec {
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+		String^ constring = L"datasource = localhost; port = 3306; username = CricRec; password = cricrec";
+		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("INSERT INTO cricrec.venue (name) values ('" + this->textBox1->Text +"');", conDataBase);
+		MessageBox::Show("Venue Added Sucessfully");
+
+		MySqlDataReader^ myReader;
+		try {
+			conDataBase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+
+			while (myReader->Read()) {
+
+			}
+		}
+		catch (Exception^ex) {
+			MessageBox::Show(ex->Message);
+		}
+
+	}
+};
 }
