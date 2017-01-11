700bca3626f2609b29dae0626b1356c9266cb0e6
MuhammadAasharib <aasharib@icloud.com>
2017-01-11 16:26:33 +0500

    umpire

--- CricRec/CreateUmpire.h
+++ CricRec/CreateUmpire.h
@@ -8,6 +8,7 @@ namespace CricRec {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
 
 	/// <summary>
 	/// Summary for CreateUmpire
@@ -36,10 +37,10 @@ namespace CricRec {
 		}
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  label3;
+
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::TextBox^  textBox2;
-	private: System::Windows::Forms::TextBox^  textBox3;
+
 	private: System::Windows::Forms::Button^  button1;
 	protected:
 
@@ -58,10 +59,8 @@ namespace CricRec {
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
@@ -79,18 +78,9 @@ namespace CricRec {
 			this->label2->AutoSize = true;
 			this->label2->Location = System::Drawing::Point(30, 65);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(16, 13);
+			this->label2->Size = System::Drawing::Size(26, 13);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"Id";
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(30, 99);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(26, 13);
-			this->label3->TabIndex = 2;
-			this->label3->Text = L"Age";
+			this->label2->Text = L"Age";
 			// 
 			// textBox1
 			// 
@@ -106,13 +96,6 @@ namespace CricRec {
 			this->textBox2->Size = System::Drawing::Size(100, 20);
 			this->textBox2->TabIndex = 4;
 			// 
-			// textBox3
-			// 
-			this->textBox3->Location = System::Drawing::Point(136, 91);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(100, 20);
-			this->textBox3->TabIndex = 5;
-			// 
 			// button1
 			// 
 			this->button1->Location = System::Drawing::Point(136, 178);
@@ -121,6 +104,7 @@ namespace CricRec {
 			this->button1->TabIndex = 6;
 			this->button1->Text = L"ok";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &CreateUmpire::button1_Click);
 			// 
 			// CreateUmpire
 			// 
@@ -128,10 +112,8 @@ namespace CricRec {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"CreateUmpire";
@@ -141,5 +123,27 @@ namespace CricRec {
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+		String^ constring = L"datasource = localhost; port = 3306; username = CricRec; password = cricrec";
+		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("INSERT INTO cricrec.umpire (Name, age) values ('" + this->textBox1->Text + "','" + this->textBox2->Text + "');", conDataBase);
+		MessageBox::Show("Umpire Added Sucessfully");
+
+		MySqlDataReader^ myReader;
+		try {
+			conDataBase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+
+			while (myReader->Read()) {
+
+			}
+		}
+		catch (Exception^ex) {
+			MessageBox::Show(ex->Message);
+		}
+
+	}
+};
 }
